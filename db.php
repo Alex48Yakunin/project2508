2828cb536df834a431fef51c6f7aaa52087da6ae
Siivekas <siivekas@yandex.ru>
2018-12-02 17:29:45 +0300

    Пофиксила db.php

--- db.php
+++ db.php
@@ -1,3 +1,13 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'root', '', 'shop2508');
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'shop2508';
+
+$mysqli = new mysqli($host, $user, $password, $db);
+$mysqli->set_charset('utf8');
+
+if ($mysqli->connect_error) {
+    die('Ошибка подключения (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+}
