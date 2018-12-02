ab97411425a72b0b76c969dad1b51374cc8a675f
Alex Yakunin <sanchezz.jakunin@rambler.ru>
2018-12-02 17:28:47 +0300

    создал файл test.php

---
+++ templates/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500,700,700i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Магазин товаров</title>
+</head>
+
+<body>
+    <header>
+        <nav class="wrapper-inner">
+            <div class="header-nav">
+                <div class="header-nav__item">
+                    <a href="" class="header-nav__link header-nav__logo"></a>
+                    <a href="" class="header-nav__link">Женщинам</a>
+                    <a href="" class="header-nav__link">Мужчинам</a>
+                    <a href="" class="header-nav__link">Детям</a>
+                    <a href="" class="header-nav__link">Новинки</a>
+                    <a href="" class="header-nav__link">О нас</a>
+                </div>
+                <div class="header-nav__item">
+                    <a href="" class="header-nav__link header-nav__link_come">Войти</a>
+                    <a href="" class="header-nav__link header-nav__link_cart">
+                        Корзина
+                        <span class="header-nav__cart-count">(0)</span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="wrapper-inner">
+
+   
