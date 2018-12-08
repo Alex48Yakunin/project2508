15bc529ad804932f272e8edb42347b096ae187ee
IgorReshetov <ivreshetov@mail.ru>
2018-12-08 17:32:09 +0300

    Добавлен Get в контролер catalog

--- controllers/catalog.php
+++ controllers/catalog.php
@@ -1,6 +1,11 @@
 <?php
 
+$collection = (((isset($_GET['collection'])) && $_GET['collection'] !== "")?$_GET['collection']:false);
+$category_id = (((isset($_GET['category_id'])) && $_GET['category_id'] !== "")?$_GET['category_id']:false);
+$order_id = (((isset($_GET['order_id'])) && $_GET['order_id'] !== "")?$_GET['order_id']:false);
+
 require_once '../models/Product.php';
 $site_page_title = 'Каталог товаров';
-$products = Product::getAll();
+$products = Product::getAll($collection, $category_id, $order_id);
+
 require_once '../views/catalog.php';
