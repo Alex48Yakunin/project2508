6a010966a880f985e254cbb658067f57ddb73ae7
Siivekas <siivekas@yandex.ru>
2018-12-09 11:29:11 +0300

    Наваяла фавикон

--- templates/header.php
+++ templates/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500,700,700i,900,900i" rel="stylesheet">
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="../css/style.css">
     <title><?php echo "$site_page_title" ?></title>
 </head>
