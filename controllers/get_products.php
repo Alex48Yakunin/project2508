3f3d7506f947d22f8ab43c565a6d7ea218ef5237
Sergey <devilserser@yandex.ru>
2018-12-15 17:05:05 +0300

    Create get_products.php

---
+++ controllers/get_products.php
@@ -0,0 +1,17 @@
+<?php
+
+$collection = (((isset($_GET['collection'])) && $_GET['collection'] !== "")?$_GET['collection']:false);
+$category_id = (((isset($_GET['category_id'])) && $_GET['category_id'] !== "")?$_GET['category_id']:false);
+$order_id = (((isset($_GET['order_id'])) && $_GET['order_id'] !== "")?$_GET['order_id']:false);
+
+require_once '../models/Product.php';
+$site_page_title = 'Каталог товаров';
+$products = Product::getAll($collection, $category_id, $order_id);
+
+require_once '../models/Collection.php';
+$collection_id = new Collection($collection);
+
+require_once '../models/Category.php';
+$categories = Category::getAll(); 
+
+echo json_encode($products);
