539366edd2cfafc58bc28eeba2d31c68bf6133ec
Sergey <devilserser@yandex.ru>
2018-12-16 16:19:10 +0300

    Получение товара

--- controllers/get_products.php
+++ controllers/get_products.php
@@ -1,14 +1,10 @@
 <?php
 
-$collection = (((isset($_GET['collection'])) && $_GET['collection'] !== "")?$_GET['collection']:false);
-$category_id = (((isset($_GET['category_id'])) && $_GET['category_id'] !== "")?$_GET['category_id']:false);
-$order_id = (((isset($_GET['order_id'])) && $_GET['order_id'] !== "")?$_GET['order_id']:false);
+$collection = (((isset($_REQUEST['collection'])) && $_REQUEST['collection'] !== "")?$_REQUEST['collection']:false);
+$category_id = (((isset($_REQUEST['category_id'])) && $_REQUEST['category_id'] !== "")?$_REQUEST['category_id']:false);
 
 require_once '../models/Product.php';
 $site_page_title = 'Каталог товаров';
-$products = Product::getAll($collection, $category_id, $order_id);
-
-$postData = file_get_contents('php://input');
-$data = json_decode($postData, true);
+$products = Product::getAll($collection, $category_id);
 
 echo json_encode($products);
--- js/product_generation.js
+++ js/product_generation.js
@@ -1,51 +1,42 @@
 $(document).ready(function () {
 
-    $('.category_list').click(function(){
-       
-        
+    var category_id = '';
+    
+    var collection = $('.collection_id').attr('id');
+    collection = collection.match(/\d+/);
+    collection = Number.parseInt(collection);
+    
+    GetProducts(category_id, collection);
+
+    $('.category_list').click(function () {
+
+        $('.products').children().remove();
+
         var category_id = $(this).attr('id');
         category_id = category_id.match(/\d+/);
         category_id = Number.parseInt(category_id);
-        
-        var collection = $('.collection_id').attr('id');
-        collection = collection.match(/\d+/);
-        collection = Number.parseInt(collection);
-        
-        console.log(category_id + ' ' + collection);
-        
-        var data = {
-            category_id: category_id,
-            collection: collection
-        }
-        
-        data = JSON.stringify(data);
-        
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'get_products.php', true);
-        xhr.setRequestHeader("Content-type", "application/json");
-        xhr.send(data);
-        
+
+        GetProducts(category_id, collection)
     });
     
     
-    
-    $.post('get_products.php', {
+});
 
+function GetProducts(category, collection) {
+    $.post('get_products.php', {
+        category_id: category,
+        collection: collection
     }, function (data) {
-        
         var products = JSON.parse(data);
-        
-        console.log(products);
-        
-        products.forEach(function(product){
+
+        products.forEach(function (product) {
             $('.products').append(
-                '<a href="../controllers/product.php?product_id='+ product.id +'" class="products-item">' + 
-                    '<div class="products-item-photo" style="background-image: url(../images/picture/'+ product.image +')"></div>' +
-                    '<p class="products-item-title">'+ product.title +'</p>' +
-                    '<p class="products-item-price">'+ product.price +'</p>' +
+                '<a href="../controllers/product.php?product_id=' + product.id + '" class="products-item">' +
+                '<div class="products-item-photo" style="background-image: url(../images/picture/' + product.image + ')"></div>' +
+                '<p class="products-item-title">' + product.title + '</p>' +
+                '<p class="products-item-price">' + product.price + '</p>' +
                 '</a>'
             );
         });
     });
-
-});
+}
