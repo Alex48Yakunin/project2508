87f380305a257309f842ac15724a2cea039045a0
Siivekas <siivekas@yandex.ru>
2018-12-22 15:02:44 +0300

    Update delete_demon.php

--- views/delete_demon.php
+++ views/delete_demon.php
@@ -3,7 +3,7 @@
   <div class="delete-banner-inner col-3 rounded">
     <p>Точно удалить?</p>
     <a href="../controllers/handler_delete.php?product_id=<?php echo $product_id; ?>" class="btn btn-danger">Да, удаляем!</a>
-    <a href="#" class="btn btn-secondary">Не уверен...</a>
+    <a href="../controllers/admin_products_list.php" class="btn btn-secondary">Не уверен...</a>
   </div>
 </div>
 <?php require_once '../templates/admin_footer.php'; ?>
