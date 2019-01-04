a9ea6cca50fe5da36a428b37b6ad973b2b80bf68
Siivekas <siivekas@yandex.ru>
2019-01-04 16:29:38 +0300

    Добавила обработчик авторизации
    Проверяет соответствие почты, пароля и хеша пароля и перенаправляет в зависимости от роли авторизовавшегося. Сессию пока не делает и ошибки не выводит.

--- controllers/handler_auth.php
+++ controllers/handler_auth.php
@@ -5,14 +5,22 @@ require_once '../models/User.php';
 $email = $_POST['email'];
 $pass = $_POST['pass'];
 
-if (!empty($login) && !empty($pass)) {
+if (!empty($email) && !empty($pass)) {
     $user = User::getByEmail($email);
     $hash = $user->pass;
+
     if(password_verify($pass, $hash)) {
-        header('Location: catalog.php');
+
+        if($user->role == 1) {
+            header('Location: admin_orders.php');
+        } else {
+            header('Location: catalog.php');
+        }
+
     } else {
         header('Location: auth.php');
     }
+
 } else {
     header('Location: auth.php');
     exit;
