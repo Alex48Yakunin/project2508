8a1469b66d333aa211de2d0a60c9f3f91818062a
Alex Yakunin <sanchezz.jakunin@rambler.ru>
2019-01-12 17:25:06 +0300

    написал вывод размеров
    нужно ещё застилить и там ошибка)

--- js/product_generation.js
+++ js/product_generation.js
@@ -6,52 +6,58 @@ $(document).ready(function () {
     collection = collection.match(/\d+/);
     collection = Number.parseInt(collection);
 
-    if (collection == 0){collection = ''};
+    if (collection == 0) {
+        collection = ''
+    };
     var price_min = Number.parseInt($('#min-price').val());
     var price_max = Number.parseInt($('#max-price').val());
     var title = '';
     var page = 1;
 
     GetProducts(category_id, collection, title, price_min, price_max, page);
-    
+
     $('#seach').click(function () {
 
         $('.products').children().remove();
         var category_id = $('#category').attr('cat');
-        category_id = ((category_id == "all")?"":Number.parseInt(category_id));
+        category_id = ((category_id == "all") ? "" : Number.parseInt(category_id));
         var collection = $('.collection_id').attr('id');
         collection = collection.match(/\d+/);
         collection = Number.parseInt(collection);
-        if (collection == 0){collection = ''};
+        if (collection == 0) {
+            collection = ''
+        };
 
         var price_min = Number.parseInt($('#min-price').val());
         var price_max = Number.parseInt($('#max-price').val());
         var title = $('#seach-title').val();
 
         GetProducts(category_id, collection, title, price_min, price_max, page);
-        $('#filter').text('ФИЛЬТР: НАИМЕНОВАНИЕ:'+ ((title =="")?'Все':"%"+title+"%")+'.    КАТЕГОРИЯ: '+ ((category_id =="")?'Все':$('#category').text())+'.    ЦЕНА: '+'от '+$('#min-price').val()+'руб.'+' до '+$('#max-price').val()+'руб.');
+        $('#filter').text('ФИЛЬТР: НАИМЕНОВАНИЕ:' + ((title == "") ? 'Все' : "%" + title + "%") + '.    КАТЕГОРИЯ: ' + ((category_id == "") ? 'Все' : $('#category').text()) + '.    ЦЕНА: ' + 'от ' + $('#min-price').val() + 'руб.' + ' до ' + $('#max-price').val() + 'руб.');
         window.history.pushState(category_id, "Title", "?collection=" + collection + "&category_id=" + category_id); // подмена url
     });
-    
+
 
     $('.pages-item').click(function () {
 
         $('.products').children().remove();
         var category_id = $('#category').attr('cat');
-        category_id = ((category_id == "all")?"":Number.parseInt(category_id));
+        category_id = ((category_id == "all") ? "" : Number.parseInt(category_id));
         var collection = $('.collection_id').attr('id');
         collection = collection.match(/\d+/);
         collection = Number.parseInt(collection);
-        if (collection == 0){collection = ''};
+        if (collection == 0) {
+            collection = ''
+        };
 
         var price_min = Number.parseInt($('#min-price').val());
         var price_max = Number.parseInt($('#max-price').val());
         var title = $('#seach-title').val();
         page = Number.parseInt($(this).text());
-    
+
 
         GetProducts(category_id, collection, title, price_min, price_max, page);
-        $('#filter').text('ФИЛЬТР: НАИМЕНОВАНИЕ:'+ ((title =="")?'Все':"%"+title+"%")+'.    КАТЕГОРИЯ: '+ ((category_id =="")?'Все':$('#category').text())+'.    ЦЕНА: '+'от '+$('#min-price').val()+'руб.'+' до '+$('#max-price').val()+'руб.');
+        $('#filter').text('ФИЛЬТР: НАИМЕНОВАНИЕ:' + ((title == "") ? 'Все' : "%" + title + "%") + '.    КАТЕГОРИЯ: ' + ((category_id == "") ? 'Все' : $('#category').text()) + '.    ЦЕНА: ' + 'от ' + $('#min-price').val() + 'руб.' + ' до ' + $('#max-price').val() + 'руб.');
         window.history.pushState(category_id, "Title", "?collection=" + collection + "&category_id=" + category_id); // подмена url
 
     });
@@ -67,17 +73,17 @@ function GetProducts(category, collection, title, price_min, price_max, page) {
         page: page
     }, function (data) {
         var data = JSON.parse(data);
-        console.log (data);
+        // console.log(data);
         var products = data.products;
-        products.forEach(function (product) {
-            $('.products').append(
-                '<a href="../controllers/product.php?product_id=' + product.id + '" class="products-item">' +
-                '<div class="products-item-photo" style="background-image: url(../images/picture/' + product.image + ')"></div>' +
-                '<p class="products-item-title">' + product.title + '</p>' +
-                '<p class="products-item-price">' + product.price + '</p>' +
-                '</a>'
-            );
-        });
+        // products.forEach(function (product) {
+        //     $('.products').append(
+        //         '<a href="../controllers/product.php?product_id=' + product.id + '" class="products-item">' +
+        //         '<div class="products-item-photo" style="background-image: url(../images/picture/' + product.image + ')"></div>' +
+        //         '<p class="products-item-title">' + product.title + '</p>' +
+        //         '<p class="products-item-price">' + product.price + '</p>' +
+        //         '</a>'
+        //     );
+        // });
 
         if (products.length == 0) {
             $('.products').append(
@@ -91,10 +97,17 @@ function GetProducts(category, collection, title, price_min, price_max, page) {
                     '<div class="products-item-photo" style="background-image: url(../images/picture/' + product.image + ')"></div>' +
                     '<p class="products-item-title">' + product.title + '</p>' +
                     '<p class="products-item-price">' + product.price + '</p>' +
+                    '<p><section>' + catalogSizes + '</section></p>' +
                     '<p class="products-item-cart"></p>' +
                     '</a>'
                 );
             });
+            var catalogSizes = '';
+            products.sizes.forEach(function (size) {
+                catalogSizes += $('.products').append(
+                    '<option data-size-id = "' + size.id + '">' + size.value + '</option>'
+                );
+            });
         }
     })
-    }
+}
