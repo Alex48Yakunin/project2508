76a84d16b69d4c2d62a330ed683baed4fb855b12
Siivekas <siivekas@yandex.ru>
2019-01-13 01:14:42 +0300

    Запустила загрузку картинки при изменении товара

--- controllers/handler_update.php
+++ controllers/handler_update.php
@@ -1,14 +1,38 @@
 <?php
 
-
-
 require_once '../models/Product.php';
+
+//собрать строку из случайных символов - стырено, но понятно
+function randomString($length) { 
+    $keys = array_merge(range(0,9), range('a', 'z'));
+
+    $key = "";
+    for($i=0; $i < $length; $i++) {
+        $key .= $keys[mt_rand(0, count($keys) - 1)];
+    }
+    return $key;
+}
+
+if($_FILES['upload_pic']['type'] == 'image/jpeg' || $_FILES['upload_pic']['type'] == 'image/png') {
+
+    //получить расширение файла
+    $extension = explode('.', $_FILES['upload_pic']['name']); //пользовательское название файла раздербанить в массив по точке
+    $_FILES['upload_pic']['name'] = randomString(10).'.'.end($extension); //переименовать файл в рандомнаястрока.расширение
+    
+    //собственно, сохранить файл на сервер
+    $path = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR.'uploaded'.DIRECTORY_SEPARATOR.$_FILES['upload_pic']['name'];
+    move_uploaded_file($_FILES['upload_pic']['tmp_name'], $path);
+} else {
+    die('Выберите JPEG или PNG.');
+}
+
 $product_id = $_REQUEST['product_id'];
 $title = $_REQUEST['title'];
 $description = $_REQUEST['description'];
 $price = $_REQUEST['price'];
 $collection = $_REQUEST['collection'];
-$category = $_REQUEST['category'];
+$category_id = $_REQUEST['category'];
+$image = '../uploaded/'.$_FILES['upload_pic']['name']; //кривовато, но костылит отсутствие единой точки входа и единой системы папок с картинками
 
 $product = new Product($product_id);
 $product->update($title, $description, $image, $price, $category_id, $collection);
--- templates/form_upload.php
+++
@@ -1,4 +0,0 @@
-<form action = "../controllers/handler_upload.php" method = "POST" enctype = 'multipart/form-data'>
-  <input type = "file"  name="upload"/>
-  <input type = "submit" value = "Загрузить"/>
-</form>
